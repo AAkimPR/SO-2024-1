3dc9197406d34a89e229d4ff8e529a539fa86744
AAkimPR <155691338+AAkimPR@users.noreply.github.com>
2024-03-18 10:52:42 -0300

    process.java

---
+++ process.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Process{
+
+  int n_Process = 3;
+
+  int[]temp_exec = new int [n_Process];
+  int[]temp_esper  =new int[n_Process];
+  int[]tempchegada = new int[n_Process];
+  int[]prioridade = new int[n_Process];
+  int[]tempo_restan = new int[n_Process];
+
